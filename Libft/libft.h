8a0c17c70ee171d07027dc8767c39e778be7df25
Vinogradova Darina <48646373+VinogradovaD@users.noreply.github.com>
2021-10-28 15:45:06 +0300

    Add files via upload

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,18 +6,21 @@
 /*   By: clockhar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 16:19:19 by clockhar          #+#    #+#             */
-/*   Updated: 2021/10/23 15:44:06 by                  ###   ########.fr       */
+/*   Updated: 2021/10/26 19:02:35 by clockhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
+# define LIBFT_H
 
-#define LIBFT_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef	unsigned long	size_t;
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 int		ft_isalpha(int ch);
 int		ft_isdigit(int ch);
 int		ft_isalnum(int ch);
@@ -35,24 +38,36 @@ int		ft_tolower(int ch);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
-void	*ft_memchr (const void *arr, int c, size_t n);
+void	*ft_memchr(const void *arr, int c, size_t n);
 int		ft_memcmp(const void *buf1, const void *buf2, size_t count);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t nitems, size_t size);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char    *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char 	**ft_split(char const *s, char c);
-int     count(char const *str, char c);
-void    *words_writing(char const *s, char **p, char c);
-char    **ft_split(char const *s, char c);
-int     is_space(char const *c);
+char	**ft_split(char const *s, char c);
+int		count(char const *s, char del);
+char	**ft_split(char const *s, char c);
+char	**words_writing(char const *s, char **p, char c, int words_count);
+char	*ft_itoa(int n);
+int		len(int n);
+void	add_num(char *str, int n, int l);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 #endif
